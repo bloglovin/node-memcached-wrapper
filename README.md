4afe515eaf5b3339cf68424fa290fcc9e69fe278
Patrik Ring <me@patrikring.se>
2013-05-23 00:27:29 +0200

    Moar docs

--- README.md
+++ README.md
@@ -3,7 +3,32 @@ node-memcached-wrapper
 
 A small memcached wrapper using node-memcached
 
-Example usage
+Documentation
+-------------
+There are 4 exposed public methods
+
+```javascript
+function get(key, callback) {}
+function set(key, value, ttl, callback) {}
+function remove(key, callback) {}
+function end() {}
+```
+
+The callback should expect one parameter, response, which will be the actual result from memcached. If false, the key did not exist, or we couldn't set the key
+
+```javascript
+function callback(resp) {
+  if (resp === false) {
+    // Nothing here to see
+  } else {
+    // Use your response
+  }
+}
+```
+
+The wrapper should make sure errors are handled in a preferable way although this far it seems like the node-memcached lib is completely incapable of handling errors at all. This is a problem.
+
+Examples
 -------------
 Init
 
--- index.js
+++ index.js
@@ -29,18 +29,42 @@ var MemcachedWrapper = function(config) {
   }
 
   return {
+    /**
+     * get
+     * @param string key
+     * @param function cb
+     */
     get: function(key,cb){
       Mc.get(key, errorHandler(cb))
     },
-    
+
+    /**
+     * set
+     * @param string key
+     * @param mixed value
+     * @param int ttl (seconds)
+     * @param function cb
+     */
     set: function(key, value, ttl, cb) {
       Mc.set(key, value, ttl, errorHandler(cb))
     },
 
+    /**
+     * remove
+     * Simply removes a key from memcached
+     * @param string key
+     * @param function cb
+     */
     remove: function(key, cb) {
       Mc.del(key, errorHandler(cb))
     },
 
+    /**
+     * end
+     * Disconnects from the cluster
+     * Might not want to use this since there's currently
+     * no way to reconnect
+     */
     end: function() {
       Mc.end()
     }
