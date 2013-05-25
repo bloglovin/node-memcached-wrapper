0b36d92a81e9e7efc92a8cb705c10cf808444a8b
Patrik Ring <me@patrikring.se>
2013-05-25 14:49:34 +0200

    Use prototype to extend object

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // @fileoverview Memcache wrapper
 
-var MemcachedClient = require('memcached')
+var MemcachedClient = require('memcached');
 
 /**
  * MemcacheWrapper
@@ -8,69 +8,72 @@ var MemcachedClient = require('memcached')
  *  config.servers
  *  config.options
  */
-var MemcachedWrapper = function(config) {
-  var Mc = null
+var MemcachedWrapper = function (config) {
+  this.Mc = null;
+  var self = this;
 
-  getConnection = function() {
-    Mc = new MemcachedClient(config.servers, config.options)
-  }
+  var getConnection = function getConnection() {
+    self.Mc = new MemcachedClient(config.servers, config.options);
+  };
 
-  getConnection()
+  getConnection();
 
   errorHandler = function(cb) {
+    if ( ! cb) {
+      var cb = function(resp) {};
+    }
+
     return function(err, resp) {
       if (err) {
-        console.log(err)
-        cb(false)
+        console.log(err);
+        cb(false);
       } else {
-        cb(resp)
+        cb(resp);
       }
-    }
-  }
-
-  return {
-    /**
-     * get
-     * @param string key
-     * @param function cb
-     */
-    get: function(key,cb){
-      Mc.get(key, errorHandler(cb))
-    },
-
-    /**
-     * set
-     * @param string key
-     * @param mixed value
-     * @param int ttl (seconds)
-     * @param function cb
-     */
-    set: function(key, value, ttl, cb) {
-      Mc.set(key, value, ttl, errorHandler(cb))
-    },
-
-    /**
-     * remove
-     * Simply removes a key from memcached
-     * @param string key
-     * @param function cb
-     */
-    remove: function(key, cb) {
-      Mc.del(key, errorHandler(cb))
-    },
-
-    /**
-     * end
-     * Disconnects from the cluster
-     * Might not want to use this since there's currently
-     * no way to reconnect
-     */
-    end: function() {
-      Mc.end()
     }
   }
 }
 
+/**
+ * get
+ * @param string key
+ * @param function cb
+ */
+MemcachedWrapper.prototype.get = function get(key,cb) {
+  this.Mc.get(key, errorHandler(cb));
+}
+
+/**
+ * set
+ * @param string key
+ * @param mixed value
+ * @param int ttl (seconds)
+ * @param function cb
+ */
+MemcachedWrapper.prototype.set = function set(key, value, ttl, cb) {
+  this.Mc.set(key, value, ttl, errorHandler(cb));
+}
+
+/**
+ * remove
+ * Simply removes a key from memcached
+ * @param string key
+ * @param function cb
+ */
+MemcachedWrapper.prototype.remove = function remove(key, cb) {
+  this.Mc.del(key, errorHandler(cb));
+}
+
+/**
+ * end
+ * Disconnects from the cluster
+ * Might not want to use this since there's currently
+ * no way to reconnect
+ */
+MemcachedWrapper.prototype.end = function end() {
+  this.Mc.end();
+}
+
 module.exports = function (config) {
-  return new MemcachedWrapper(config)
+  return new MemcachedWrapper(config);
 }
